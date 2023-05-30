47835644b43ed7ed85b5401b6822b124d5d3674f
Davor Lakuš <lakus.davor@gmail.com>
2023-05-30 15:54:01 +0200

    first commit

---
+++ Noisy/NoisyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoisyApp.swift
+//  Noisy
+//
+//  Created by Davor Lakus on 30.05.2023..
+//
+
+import SwiftUI
+
+@main
+struct NoisyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
